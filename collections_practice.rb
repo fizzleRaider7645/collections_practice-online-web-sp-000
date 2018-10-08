c994e433ba166f122e66f1ebc46b318ae99a4092
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 13:47:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,6 +16,7 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
+  og_array = array
   array[index] = array[destination_index]
   array[destination_index] = array[index]
   array
